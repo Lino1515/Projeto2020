a9cdd1255ce3512c1b9702dcf6edae82f85e8d56
Dinas <leopoldo31920@gmail.com>
2020-11-20 16:47:14 +0000

    sidebar +\-

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this \yii\web\View */
 /* @var $content string */
 
@@ -9,102 +8,126 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
+use kartik\sidenav\SideNav;
 
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
-    <?php
-use kartik\sidenav\SideNav;
-     
-echo SideNav::widget([
-	'type' => SideNav::TYPE_DEFAULT,
-	'heading' => 'Options',
-	'items' => [
-		[
-			'url' => '#',
-			'label' => 'Home',
-			'icon' => 'home'
-		],
-		[
-			'label' => 'Help',
-			'icon' => 'question-sign',
-			'items' => [
-				['label' => 'About', 'icon'=>'info-sign', 'url'=>'#'],
-				['label' => 'Contact', 'icon'=>'phone', 'url'=>'#'],
-			],
-		],
-	],
-]);?>
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = ['label' => 'Tipo Jogo', 'url' => ['/tipojogo/index']];
-        $menuItems[] = ['label' => 'Jogos', 'url' => ['/jogos/index']];
-        $menuItems[] = ['label' => 'User', 'url' => ['/user/index']];
-        $menuItems[] = ['label' => 'Comentarios', 'url' => ['/comentarios/index']];
-        $menuItems[] = ['label' => 'Review', 'url' => ['/review/index']];
-        $menuItems[] = ['label' => 'Creports', 'url' => ['/comentariosreports/index']];
-        $menuItems[] = ['label' => 'Cutilizador', 'url' => ['/comentariosutilizador/index']];
-        $menuItems[] = ['label' => 'Rutilizador', 'url' => ['/reviewutilizador/index']];
-        $menuItems[] = ['label' => 'Rreports', 'url' => ['/reviewreports/index']];
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php $this->registerCsrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body style="overflow:auto;">
+        <?php $this->beginBody() ?>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</div>
+        <div class="wrap">
+            <?php
+            NavBar::begin([
+                'brandLabel' => 'IGDb',
+                'brandUrl' => Yii::$app->homeUrl,
+                'innerContainerOptions' => ['class' => 'container-fluid'],
+                'options' => [
+                    'class' => 'navbar-inverse navbar-fixed-top ',
+                ],
+            ]);
+            $menuItemsNav = [['label' => 'Home', 'url' => ['/site/index']]];
+            if (Yii::$app->user->isGuest) {
+                $menuItemsNav[] = ['label' => 'Login', 'url' => ['/site/login']];
+            } else {
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+                $menuItemsNav[] = '<li>'
+                        . Html::beginForm(['/site/logout'], 'post')
+                        . Html::submitButton(
+                                'Logout (' . Yii::$app->user->identity->username . ')', ['class' => 'btn btn-link logout']
+                        )
+                        . Html::endForm()
+                        . '</li>';
+            }
+            echo Nav::widget([
+                'options' => ['class' => 'navbar-nav navbar-right'],
+                'items' => $menuItemsNav,
+            ]);
+            NavBar::end();
+            ?>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+            <div class="container-fluid" style="padding-left: 0px; overflow: hidden; ">
+                <div class="sidebarmain">
+                    <?php
+                    echo SideNav::widget([
+                        //'type' => SideNav::TYPE_DEFAULT,
+                        //'heading' => 'Options',
+                        'options' => ['class' => 'sidebarstyle', 'style' => ''],
+                        'items' => [
+                            [
+                                'label' => 'Tipo Jogo',
+                                'items' => [
+                                    ['label' => 'Lista', 'icon' => 'info-sign', 'url' => ['/tipojogo/index']],
+                                    ['label' => 'Criar', 'icon' => 'phone',  'url' => ['/tipojogo/create']],
+                                ],
+                                'url' => ['/tipojogo/index'],
+                            ],
+                            [
+                                'label' => 'Jogos',
+                                'url' => ['/jogos/index']
+                            ],
+                            [
+                                'label' => 'User',
+                                'url' => ['/user/index']
+                            ],
+                            [
+                                'label' => 'Comentarios',
+                                'url' => ['/comentarios/index']],
+                            [
+                                'label' => 'Review',
+                                'url' => ['/review/index']
+                            ],
+                            [
+                                'label' => 'Creports',
+                                'url' => ['/comentariosreports/index']
+                            ],
+                            [
+                                'label' => 'Cutilizador',
+                                'url' => ['/comentariosutilizador/index']
+                            ],
+                            [
+                                'label' => 'Rutilizador',
+                                'url' => ['/reviewutilizador/index']
+                            ],
+                            [
+                                'label' => 'Rreports',
+                                'url' => ['/reviewreports/index']
+                            ],
+                        ],
+                    ]);
+                    ?>
+                </div>
+                <div class="content_text">
+                    <?=
+                    Breadcrumbs::widget([
+                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                    ])
+                    ?>
+                    <?= Alert::widget() ?>
+                    <?= $content ?>
+                </div>
+            </div>
+        </div>
 
-<?php $this->endBody() ?>
-</body>
+        <footer class="footer">
+            <div class="container">
+                <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+
+                <p class="pull-right"><?= Yii::powered() ?></p>
+            </div>
+        </footer>
+
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
